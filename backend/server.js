9d287b64eb3fbcb141b2fc06c415f666099ab0b2
pppllll084-cmd <pppllll084@gmail.com>
2025-10-08 23:08:56 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -71,4 +71,5 @@ app.get("/availability", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
